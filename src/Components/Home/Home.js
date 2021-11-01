7fde4fce50e6485c0bfe0b51b30e446b85710efe
kushan madhusanka <kusham1998@gmail.com>
2021-11-01 19:52:11 +0530

    changes responssivelly

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -19,25 +19,25 @@ const Home = () => {
       <section className="home">
         <div className="comming-soon row d-flex align-items-stretch">
           <div className="numberCount col-md-9 col-sm-9 col-xs-9 col-8">
-            <div className="container-day">
-              <h3 className="day">00</h3>
+            <div className="container-day col-md-3 col-sm-3 col-xs-3 col-3">
+              <h3 className="day h3">00</h3>
               <h4>Day</h4>
             </div>
-            <div className="container-hour">
+            <div className="container-hour col-md-3 col-sm-3 col-xs-3 col-3">
               <h3 className="hour">00</h3>
               <h4>hour</h4>
             </div>
-            <div className="container-minute">
+            <div className="container-minute col-md-3 col-sm-3 col-xs-3 col-3">
               <h3 className="minute">00</h3>
               <h4>minute</h4>
             </div>
-            <div className="container-second">
+            <div className="container-second col-md-3 col-sm-3 col-xs-3 col-3">
               <h3 className="second">00</h3>
               <h4>second</h4>
             </div>
           </div>
 
-          <div className="container-register col-md-3 col-sm-3 col-xs-3 col-3">
+          <div className="container-register col-md-3 col-sm-3 col-xs-3 col-4">
             <a href="#x">Register Now</a>
           </div>
         </div>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,33 +1,46 @@
-import React from "react";
-import './NavBar.css'
+import React, { useState } from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
-    
+  const [show, setshow] = useState(false);
+  const showNavBar = () => {
+    setshow(!show);
+  };
   return (
     <div>
       <nav>
         <div className="navBar row">
-          <div className="logo col-md-3 col-sm-3">
+          <div className="logo col-3 col-md-3 col-sm-3">
             <a href="#x">ACM</a>
           </div>
-          <div className="nav-links col-md-9 col-sm-9">
+          <div
+           
+            className="menuIcon col-9 col-md-9 col-sm-9"
+          >
+            <i  onClick={showNavBar} class="fas fa-align-justify"></i>
+          </div>
+          <div
+            className={
+              show ? "col-md-12 col-sm-12" : "nav-links col-md-9 col-sm-9"
+            }
+          >
             <div className="row">
-              <div className= "items active col-md-2 col-sm-2" >
+              <div className="items active col-md-2">
                 <a href="#x">Home</a>
               </div>
-              <div className="items col-md-2 col-sm-2">
+              <div className="items col-md-2 ">
                 <a href="#x">sponsers</a>
               </div>
-              <div className="items col-md-2 col-sm-2">
-                <a href="#x">pricing</a>
+              <div className="items col-md-2">
+                <a href="#xgh">pricing</a>
               </div>
-              <div className="items col-md-2 col-sm-2">
+              <div className="items col-md-2">
                 <a href="#x">FAQ</a>
               </div>
-              <div className="items col-md-2 col-sm-2">
+              <div className="items col-md-2">
                 <a href="#x">ContactUs</a>
               </div>
-              <div className="items col-md-2 col-sm-2">
+              <div className="items col-md-2">
                 <a href="#x">aboutUs</a>
               </div>
             </div>
